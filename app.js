3539a92b66047de71563d0b1e1df25310b4b1b10
MatasMM <153711311+MatasMM@users.noreply.github.com>
2024-03-01 12:01:33 +0200

    commit

--- app.js
+++ app.js
@@ -6,12 +6,50 @@ function main () {
    bmi = bmi.toFixed(2);
 
   if (bmi > 0 && bmi <= 18.5) {
-    document.getElementById('output').innerHTML = 'Your BMI is : <b>' + bmi + '</b><br>Your weight is <span>Underweight</span>.';
+    document.getElementById('output').innerHTML = 'Your BMI is : <b>' + bmi;
+    output.style.color ='Gold';
   } else if (bmi > 18.5 && bmi <= 25) {
-    document.getElementById('output').innerHTML = 'Your BMI is : <b>' + bmi + '</b><br>Your weight is <span>Normal (healthy weight)</span>.';
+    document.getElementById('output').innerHTML = 'Your BMI is : <b>' + bmi;
+    output.style.color ='green';
   } else if (bmi > 25 && bmi <= 40) {
-    document.getElementById('output').innerHTML = 'Your BMI is : <b>' + bmi + '</b><br>Your weight is <span>Overweight</span>.';
+    document.getElementById('output').innerHTML = 'Your BMI is : <b>' + bmi;
+    output.style.color ='red';
   } else {
     document.getElementById('output').innerHTML = 'Please type valid numbers.';
   }
 }
+
+
+const isDisabled = () => {
+  console.log("isDisabled");
+const height = parseFloat(document.getElementById("Height").value);
+const weight = parseFloat(document.getElementById("Weight").value);
+const clearBtn = document.getElementById("clearBtn");
+
+ clearBtn.classList.add("active");
+
+ 
+  if (height || weight) {
+
+       clearBtn.disabled = false
+ 
+  } else {
+     
+      clearBtn.disabled = true;
+  }
+ 
+ 
+};
+
+
+const clearBtn = document.querySelector('.btn1')
+function clear() {
+
+  document.getElementById("Height").value = null;
+  document.getElementById("Weight").value = null;
+ 
+  clearBtn.classList.remove('active');
+ 
+}
+
+clearBtn.addEventListener('click', clear);
